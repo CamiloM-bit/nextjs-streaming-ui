c96259d1fe1e9111cdcbfd6edf0963af9ce16e66
Camilo Meza <mezacamilo841@gmail.com>
2026-02-21 11:42:51 -0500

    Modificacion en Navbar.tsx, y creacion de la ruta /Collections

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -22,8 +22,8 @@ export default function Navbar() {
 
   return (<>
     <header className="flex items-center border border-orange-800  fixed  h-[12%] w-full" >
-      <div className="border border-red-800  w-[15%] h-full">
-        <div className="border border-orange-500 h-full content-center"> {/* Logo Container */}
+      <div className="border border-red-800  flex-1 h-full">
+        <div className="border border-orange-500 h-full w-[50%] content-center"> {/* Logo Container */}
           <div>
             <figure className="border border-red-500 m-2.5 flex justify-center">
               <img className="w-37.5 text-center"  src="https://tv.selectra.com/sites/tv.selectra.com/files/styles/article_hero/public/images/logos/netflix-logo.png?itok=fCUER-69" alt="Logo" />
@@ -31,7 +31,7 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-      {<nav className="flex-1 border border-blue-400  h-full content-center pl-6">
+      {<nav className="border border-blue-400  h-full w-[60%] content-center pl-6">
         <Link href="/es" onFocus={() => { setActive("/es")           // cambia visual Rapido 
           router.replace("/es")       // Remplaza la ruta real 
         }}
@@ -76,6 +76,16 @@ export default function Navbar() {
           ${active === "/MiLista"
               ? " text-white"
               : "hover:text-white"}`}>My List</Link> {" "}
+
+
+              <Link href="/Collections" onFocus={() => {
+          setActive("/Collections")           
+          router.replace("/Collections")      
+        }}
+          className={`outline-0 p-1.5 px-3  text-gray-300 transition
+          ${active === "/Collections"
+              ? " text-white"
+              : "hover:text-white"}`}>Collections</Link>{" "}    
       </nav>}
       <div className="flex-1 border border-blue-700">
 
