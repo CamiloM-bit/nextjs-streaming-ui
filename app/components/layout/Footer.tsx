5066f6e03a8578e29e60182f96f128622a24ccb5
Camilo Meza <mezacamilo841@gmail.com>
2026-03-06 13:59:06 -0500

    feat(components): crear componente Footer con estructura inicial

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,17 @@
+
+import "../../globals.css";
+
+
+
+export default function Footer() {
+ 
+  return (
+  
+  <>
+   <section className="border border-red-500 h-[30rem] bg-red z-99 w-full">
+    <h1>footer</h1>
+   </section>
+  </>
+
+  );
+}
